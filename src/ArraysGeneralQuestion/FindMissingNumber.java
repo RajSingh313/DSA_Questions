f6e2091f5718951d9bc1d412068aedd41b672f57
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-13 22:54:47 +0500

    Find missing number

---
+++ src/ArraysGeneralQuestion/FindMissingNumber.java
@@ -0,0 +1,19 @@
+package ArraysGeneralQuestion;
+
+public class FindMissingNumber {
+    static int missingNumber(int[] arr, int number) {
+        int N = number + 1;
+int totalSum =N*(N+1)/2;
+int arrSum=0;
+for(int i =0 ; i<number; i++){
+arrSum+=arr[i];
+}
+return totalSum-arrSum;
+    }
+
+    public static void main(String[] args) {
+int[] arr ={1,2,4,5};
+        int missingNumber = missingNumber(arr, arr.length);
+        System.out.println(missingNumber);
+    }
+}
