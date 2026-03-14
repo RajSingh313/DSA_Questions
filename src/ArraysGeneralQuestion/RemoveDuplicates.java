bb08dee11732ea3d8d4c83078a67585021ea3def
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-14 20:33:47 +0500

    Problem to remove duplicates from array

---
+++ src/ArraysGeneralQuestion/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package ArraysGeneralQuestion;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 4, 5, 4, 3, 6};
+        //Use Hashset When you don't want  insertion order
+        //Use LinkedHashSet When you want Insertion order
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!set.contains(arr[i])) {
+                set.add(arr[i]);
+            }
+        }
+        System.out.println(set);
+    }
+}
