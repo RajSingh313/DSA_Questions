ed18b3a618bf32ac5459a064e89e4aafb4c66032
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-15 20:44:19 +0500

    Moving zero in End of the array

---
+++ src/ArraysGeneralQuestion/MoveZerosInEnd.java
@@ -0,0 +1,25 @@
+package ArraysGeneralQuestion;
+
+import java.util.Arrays;
+
+public class MoveZerosInEnd {
+    static void moveZeros(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 0, 6, 7, 0, 8, 0, 3, 2};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        moveZeros(arr);
+        System.out.println("After Moving zero in end" + Arrays.toString(arr));
+    }
+}
