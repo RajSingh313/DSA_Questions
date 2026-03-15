3bcfe30af96691dcec4fce48954c3545ff0799c9
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-15 21:09:55 +0500

    Moving zeros to end of the array

---
+++ src/ArraysGeneralQuestion/MovingNegativeNumberInEnd.java
@@ -0,0 +1,25 @@
+package ArraysGeneralQuestion;
+
+import java.util.Arrays;
+
+public class MovingNegativeNumberInEnd {
+    static void moveNegatives(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, -5, -4, 6, -7};
+        System.out.println("Orginal: " + Arrays.toString(arr));
+        moveNegatives(arr);
+        System.out.println("After Moving Negative Numbers: " + Arrays.toString(arr));
+
+    }
+}
