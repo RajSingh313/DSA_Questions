ab1d2b2d7c9752c4ad896a44bdc62f897dd83ad6
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-15 20:59:36 +0500

    Even Number sum without repeating numbers

---
+++ src/ArraysGeneralQuestion/EvenNumberSumWithoutRepeating.java
@@ -0,0 +1,21 @@
+package ArraysGeneralQuestion;
+
+import java.util.HashSet;
+
+public class EvenNumberSumWithoutRepeating {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 5, 6, 7, 8, 2, 2, 2, 2, 4, 4};
+        int evenSum = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (num % 2 == 0 && !set.contains(num)) {
+                evenSum += num;
+                set.add(num);
+
+            }
+        }
+        System.out.println("Total Even Numbers Sum: " + evenSum);
+
+    }
+}
