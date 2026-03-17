dd74d51d7f9963a629024db25f6077c4d5c632ad
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-17 10:25:45 +0500

    Swaping two numbers using XOR

---
+++ src/ArraysGeneralQuestion/SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+package ArraysGeneralQuestion;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println( "a: " + a);
+        System.out.println("b: " + b);
+
+        System.out.println("-------------------");
+        a = a^b; //8^5
+        b= b^a; //5^8^5= 8
+        a= a^b; //5^8^8 = 5
+        System.out.println( "a: " + a);
+        System.out.println("b: " + b);
+
+    }
+}
