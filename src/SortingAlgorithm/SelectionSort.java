121b37691f336d77c557b65b0628982fe2cee895
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-03-26 08:58:31 +0500

    Implement Selection Sort to sort an array

---
+++ src/SortingAlgorithm/SelectionSort.java
@@ -0,0 +1,32 @@
+package SortingAlgorithm;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)//this loop is for number of passes
+        {
+            int minIndex = i; // let suppose minium index is at i
+            for (int j = i + 1; j < arr.length; j++) // this will traverse array
+            {
+                if (arr[j] < arr[minIndex])// this if statement finding minium vakue inarray
+                {
+                    minIndex = j; // and assigning that values index psotion to minIndex
+
+                }
+
+            }
+            int temp = arr[minIndex]; // here we swaping founds value
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+
+        }
+
+        return arr;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,2,4,2,4,5,6,4};
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+}
