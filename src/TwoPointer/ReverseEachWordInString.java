822643ad70dcb4c99ce0d23f2a21e8c34013ac25
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-04-21 20:58:35 +0500

    reverse each word in string

--- src/TwoPointer/ReverseEachWordInString.java
+++ src/TwoPointer/ReverseEachWordInString.java
@@ -2,6 +2,21 @@ package TwoPointer;
 
 public class ReverseEachWordInString {
     public static void main(String[] args) {
+        String input = "Hello World Java";
 
+        String result = reverseEachWord(input);
+        System.out.println(result);
+    }
+
+    public static String reverseEachWord(String str) {
+        String[] words = str.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedString.append(reversedWord.reverse()).append(" ");
+        }
+
+        return reversedString.toString().trim();
     }
 }
