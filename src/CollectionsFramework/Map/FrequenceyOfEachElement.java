5ae5530283cb3bd129a91c63ef76d945490b88c6
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-05-02 18:16:06 +0500

    solve dsa question

---
+++ src/CollectionsFramework/Map/FrequenceyOfEachElement.java
@@ -0,0 +1,16 @@
+package CollectionsFramework.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequenceyOfEachElement {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 2, 3, 1, 4, 2};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        System.out.println(map);
+    }
+}
